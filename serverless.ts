868d3236b4efdbc86abce6147df756cc4ad63a26
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-12-09 17:22:12 -0300

    Gerando o PDF

--- serverless.ts
+++ serverless.ts
@@ -52,6 +52,7 @@ const serverlessConfiguration: AWS = {
       define: { 'require.resolve': undefined },
       platform: 'node',
       concurrency: 10,
+      external: ['chrome-aws-lambda'],
     },
     dynamodb: {
       stages: ['dev', 'local'],
--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,9 +1,10 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { document } from '../utils/dynamodbClient';
 import { compile } from 'handlebars';
-import * as dayjs from 'dayjs';
+import dayjs from 'dayjs';
 import { join } from 'path';
 import { readFileSync } from 'fs';
+import chromium from 'chrome-aws-lambda';
 
 interface ICreateCertificate {
   id: string;
@@ -64,6 +65,25 @@ export const handler: APIGatewayProxyHandler = async (event) => {
   };
 
   const content = await compileTemplate(data);
+
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+  });
+
+  const page = await browser.newPage();
+
+  await page.setContent(content);
+  const pdf = await page.pdf({
+    format: 'a4',
+    landscape: true,
+    printBackground: true,
+    preferCSSPageSize: true,
+    path: process.env.IS_OFFLINE ? './certificate.pdf' : null,
+  });
+
+  await browser.close();
 
   return {
     statusCode: 201,
