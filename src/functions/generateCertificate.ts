1e54735f717d8520a913d23db27d562f2407565e
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-12-09 17:04:50 -0300

    Criando template

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,11 +1,31 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { document } from '../utils/dynamodbClient';
+import { compile } from 'handlebars';
+import * as dayjs from 'dayjs';
+import { join } from 'path';
+import { readFileSync } from 'fs';
 
 interface ICreateCertificate {
   id: string;
   name: string;
   grade: string;
 }
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  medal: string;
+  date: string;
+}
+
+const compileTemplate = async (data: ITemplate) => {
+  const filePath = join(process.cwd(), 'src', 'templates', 'certificate.hbs');
+
+  const html = readFileSync(filePath, 'utf8');
+
+  return compile(html)(data);
+};
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
@@ -31,6 +51,19 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       },
     })
     .promise();
+
+  const medalPath = join(process.cwd(), 'src', 'templates', 'selo.png');
+  const medal = readFileSync(medalPath, 'base64');
+
+  const data: ITemplate = {
+    name,
+    id,
+    grade,
+    date: dayjs().format('DD/MM/YYYY'),
+    medal,
+  };
+
+  const content = await compileTemplate(data);
 
   return {
     statusCode: 201,
